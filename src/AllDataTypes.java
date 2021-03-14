63d3714759a2d5c9879ef3e5e19784bd90cde25d
Levon <lev.soghomonyan@gmail.com>
2021-03-14 18:06:30 -0400

    java: incompatible types: possible lossy conversion from int to byte

---
+++ src/AllDataTypes.java
@@ -0,0 +1,10 @@
+public class AllDataTypes {
+ public static void main (String[]argx)
+ {
+     byte number=129;
+     short digit =32768;
+     int score=24;
+     System .out.println(digit);
+ }
+
+}
