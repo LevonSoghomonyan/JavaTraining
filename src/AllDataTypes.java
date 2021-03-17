118e6b36b64b3d3bd8b78968531c7fb36a257440
Levon <lev.soghomonyan@gmail.com>
2021-03-17 09:49:45 -0400

    Float and Char to be clarified

--- src/AllDataTypes.java
+++ src/AllDataTypes.java
@@ -5,8 +5,18 @@ public class AllDataTypes {
      short digit =32767;
      int score=24;
      long score1 =1256;
-     System .out.println(digit);
+     float decimal=12;
+     double dec=1.258;;
+     boolean bl=false;
+     char symbol=13;
+     System .out.println(number);
+     System.out.println(digit);
+     System.out.println(score);
      System.out.println(score1);
+     System.out.println(decimal);
+     System.out.println(dec);
+     System.out.println(bl);
+     System.out.print(symbol);
  }
 
 }
