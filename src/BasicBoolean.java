d840407322d477963e08f46e216f24eb0e500bd1
Levon <lev.soghomonyan@gmail.com>
2021-03-18 11:11:05 -0400

    Basic Boolean false

---
+++ src/BasicBoolean.java
@@ -0,0 +1,9 @@
+public class BasicBoolean {
+    public static void main(String[]args){
+        int a=58;
+        int b=87;
+        Boolean areEquals= a==b;
+        System.out.println(areEquals);
+    }
+
+}
