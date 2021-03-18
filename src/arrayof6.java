f3d2ee81868821a95d0f777e7e998ff33b481382
Levon <lev.soghomonyan@gmail.com>
2021-03-18 15:24:39 -0400

    Citiies in array

---
+++ src/arrayof6.java
@@ -0,0 +1,15 @@
+public class arrayof6 {
+
+    public static void main(String[]args){
+        String[] cities=new String[6];
+        cities[0]="Yerevan";
+        cities[1]="Tprpnto";
+        cities[2]="Montreal";
+        cities[3]="Paris";
+
+        System.out.println(cities[0]);
+        System.out.print(cities[3]);
+        System.out.println(cities[5]);
+
+    }
+}
