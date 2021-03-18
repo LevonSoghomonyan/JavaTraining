b4951b4f130df3520c30c5edce243cea55b1ce27
Levon <lev.soghomonyan@gmail.com>
2021-03-18 16:09:33 -0400

    Conditional expression

---
+++ src/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main (String[]args){
+        int price =100;
+        if(price>1200)
+        {System.out.println("too expensive");}
+        else if(price==1200)
+        {System.out.println("still acceptable"); }
+        else
+        {System.out.print("I dont care");}
+
+    }
+}
