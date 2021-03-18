722dc51e11e30d8831bb7fa67f3c7246cd4c9472
Levon <lev.soghomonyan@gmail.com>
2021-03-18 15:53:06 -0400

    Citiies in array

---
+++ src/Predifinedarrays.java
@@ -0,0 +1,10 @@
+public class Predifinedarrays {
+    public static void main(String[]args){
+        String[] cars ={"BMW","Mercedes","Audi","Porsche"};
+        System.out.println(cars[0]);
+        System.out.println(cars[1]);
+        System.out.println(cars[3]);
+
+
+    }
+}
