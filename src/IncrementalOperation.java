bea03755383707fb5a729ff42a99d8be13ac4c20
Levon <lev.soghomonyan@gmail.com>
2021-03-20 12:18:18 -0400

    sum increment

---
+++ src/IncrementalOperation.java
@@ -0,0 +1,11 @@
+public class IncrementalOperation {
+    public static void main(String[]args){
+        double a=29;
+
+        a/=5;
+        a*=10;
+        a-=1;
+        a+=15;
+         System.out.println(a);
+    }
+}
