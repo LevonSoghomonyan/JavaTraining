955302780d6bb0d727e51c94bd75b69f2c3a4230
Levon <lev.soghomonyan@gmail.com>
2021-03-20 13:29:34 -0400

    Switch

---
+++ src/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int age = 1;
+
+        while (age < 21) {
+            System.out.println("NoAlchohol " + age + " years");
+            age++;
+
+        }
+        System.out.println("Cheers");
+    }
+}
