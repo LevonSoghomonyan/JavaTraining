6705eed33181bd70fccd8a3355e0975ad7a4a0e7
Levon <lev.soghomonyan@gmail.com>
2021-03-20 13:44:16 -0400

    For loop identic to while example

---
+++ src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int age = 1; age < 21; age+=2) {
+            System.out.println("NoAlchohol " + age + " years");
+        }
+        System.out.println("Cheers");
+    }
+}
