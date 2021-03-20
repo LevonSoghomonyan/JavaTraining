cc5c678fd4896cf6e544ccce3c8fcd72ecbd830c
Levon <lev.soghomonyan@gmail.com>
2021-03-20 12:50:44 -0400

    array of arrays

---
+++ src/ArrayOfArrays.java
@@ -0,0 +1,13 @@
+public class ArrayOfArrays {
+   public static void main(String[]args) {
+       int [] [] matrix =new int[4][2];// 2d matrix and dimensions
+       matrix[0][0]=1;
+       matrix[0][1]=-1;
+       matrix[1][0]=2;
+       matrix[1][1]=-2;
+       matrix[2][0]=3;
+       matrix[2][1]=-3;
+       System.out.println(matrix);
+
+   }
+}
