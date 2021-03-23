08021711e0e15a1dff9018709345f7f655acdb12
Levon <lev.soghomonyan@gmail.com>
2021-03-23 14:37:27 -0400

    Unable print 2*1=2,2*2=4

---
+++ src/Multiplication.java
@@ -0,0 +1,14 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int[]productions=new int [22];
+        for (int i=1;i<=21;i++)
+
+
+        {
+            //System.out.println(i);
+            System.out.println("N"+"=" +i*2);
+        }
+    }
+   // int N;
+    //N=i*2;
+}
