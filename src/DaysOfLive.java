98d90d77f1f2c826b04f60279a47f92e3323513f
Levon <lev.soghomonyan@gmail.com>
2021-03-29 21:51:23 -0400

    Write a function which will get parameter age (int) and will return converted to days of live (int).

---
+++ src/DaysOfLive.java
@@ -0,0 +1,8 @@
+public class DaysOfLive {
+    public static void main(String[]args) {
+        int years=10;
+        char days=365;
+        int howmanydays =years * days;
+                System.out.println(howmanydays);
+    }
+}
