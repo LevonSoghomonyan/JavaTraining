8498e168822399897bc929f924f3dbe12bf40052
Levon <lev.soghomonyan@gmail.com>
2021-03-29 21:59:36 -0400

    Write a function which will get age and return is the person allowed to enter the club (allows for people older the 18)

---
+++ src/EntranceToClub.java
@@ -0,0 +1,16 @@
+public class EntranceToClub {
+    public static void main(String[] args) {
+        int age = 1;
+
+        while (age < 18) {
+            System.out.println("No Enterance for  " + age + " years old ");
+            age++;
+
+        }
+        System.out.println("Welcome to our club");
+
+    }
+}
+
+
+
